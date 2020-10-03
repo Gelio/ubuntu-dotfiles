537dbcb4cb54ab15c7124d4c185014048ddac86c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-10-03 12:21:55 +0200

    add docker installation script

---
+++ install/docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# https://docs.docker.com/engine/install/ubuntu/
+
+sudo apt update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
+docker -h
