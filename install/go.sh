92f3a294e11cd406af9c16624e4d9cb4f5b360b8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2020-12-30 18:20:51 +0100

    add go installation script

---
+++ install/go.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+$FILE=go1.15.6.linux-amd64.tar.gz
+go version 2> /dev/null
+if [ $? -eq 0]; then
+	$GO_INSTALLED=1
+	sudo rm -rf /usr/local/go
+else
+	$GO_INSTALLED=0
+fi
+
+wget https://golang.org/dl/$FILE
+sudo tar -C /usr/local -xzf $FILE
+rm $FILE
+
+if [ $GO_INSTALLED -eq 0 ]; then
+	echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile
+fi
+
+/usr/local/bin/go version
+
+echo "Go should be installed. Run 'source ~/.profile' to start using it"
