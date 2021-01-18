0b8525c2429a9e2bae2c963ce1bd2a4a81e3f009
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-01-18 19:19:27 +0100

    add neovim config and adjust install script to use config

--- install/neovim.sh
+++ install/neovim.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+CONFIGS_DIR=$(dirname $PWD)/configs
+
 mkdir -p ~/.config
 cd ~/.config
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 chmod u+x nvim.appimage
 sudo ln -s "$PWD/nvim.appimage" /usr/bin/nvim
+
+# Add symlink to config
 mkdir -p nvim
-echo "set relativenumber\
-set number" >> init.vim
+ln -s "$CONFIGS_DIR/neovim.vim" nvim/init.vim
 
 # Necessary for clipboard support
 sudo apt install xsel
 
 git config --global core.editor "nvim"
+echo "EDITOR=nvim" >> ~/.profile
