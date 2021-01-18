89ae858e58b2c1d705a8e4de1eb02fac8b6840ab
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-01-18 10:05:28 +0100

    add neovim installation script

---
+++ install/neovim.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p ~/.config
+cd ~/.config
+curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
+chmod u+x nvim.appimage
+sudo ln -s "$PWD/nvim.appimage" /usr/bin/nvim
+mkdir -p nvim
+echo "set relativenumber\
+set number" >> init.vim
+
+git config --global core.editor "nvim"
