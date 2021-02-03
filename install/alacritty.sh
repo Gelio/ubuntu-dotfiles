f0f001c8128491b47913a0f605ec44a7a66b16fa
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-03 18:05:06 +0100

    add Dracula theme to alacritty

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -2,3 +2,8 @@
 
 sudo add-apt-repository ppa:aslatter/ppa
 sudo apt install alacritty
+
+CONFIGS_DIR=$(dirname $PWD)/configs
+
+mkdir -p ~/.config/alacritty
+ln -s "$CONFIGS_DIR/alacritty.yml" ~/.config/alacritty/alacritty.yml
