78c1fbef587c061159884d01e9cb4a31d0e2a37e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-03 19:34:32 +0100

    add info about wmctrl to alacritty

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -7,3 +7,5 @@ CONFIGS_DIR=$(dirname $PWD)/configs
 
 mkdir -p ~/.config/alacritty
 ln -s "$CONFIGS_DIR/alacritty.yml" ~/.config/alacritty/alacritty.yml
+
+echo "If you're on Ubuntu, add 'wmctrl -a alacritty' keyboard shortcut"
