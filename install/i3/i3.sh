f1a79fca7cf4778bddf683770e862d8b68507349
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-06 22:11:53 +0100

    add initial partial i3 config
    Workspaces are configured correctly
    Polybar does not work well and the fonts are not configured.
    Overall, the i3 bar needs to be adjusted

---
+++ install/i3/i3.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+I3_ADDITIONAL_PACKAGES=lxappearance compton hsetroot rofi xsettingsd \
+	fonts-font-awesome
+POLYBAR_PACKAGES=polybar mpd
+SCREENSAVER_PACKAGES=xscreensaver xscreensaver-gl-extra
+ROFIMOJI_PACKAGES=python3 python3-pip xdotool
+sudo apt install i3 $I3_ADDITIONAL_PACKAGES \
+	$ROFIMOJI_PACKAGES \
+	$POLYBAR_PACKAGES \
+	$SCREENSAVER_PACKAGES
+mkdir -p ~/.config/i3
+ln -s $PWD/i3_config ~/.config/i3/config
+echo "Run lxappearance and set the theme (perhaps Adwaita-dark)"
+
+# https://github.com/Mange/rofi-emoji
+echo "Installing rofimoji"
+mkdir -p ~/.local/share
+cd ~/.local/share
+git clone git@github.com:fdw/rofimoji.git
+cd rofimoji
+sudo python3 setup.py install
+cd extractors
+pip install -r requirements.txt
+echo "rofimoji installed"
+
+# xscreensaver
+echo "Configure screensaver using 'xscreensaver-demo'"
+echo "1. Create a single directory, and put the screensaver image there."
+echo "2. Select the GLSlideshow screensaver, and point it to that directory."
+echo "3. In the Advanced settings for GLSlideshow, do: 'glslideshow -root -fade 0 -zoom 100'"
+
+# Wallpaper
+echo "Configure wallpaper using feh:"
+echo "feh --bg-center [path to wallpaper]"
+
+# Add xsettingsd config
+echo "Adding a symlink to ~/.xsettingsd"
+ln -s $PWD/xsettingsd ~/.xsettingsd
+
+# Add compton config
+echo "Adding a symlink to ~/.config/compton.conf"
+ln -s $PWD/compton.conf ~/.config/compton.conf
+
+# https://github.com/polybar/polybar/wiki/Configuration
+echo "Installing polybar"
+mkdir -p ~/.local/share/polybar
+ln -s $PWD/launch_polybar.sh ~/.local/share/polybar/launch_polybar.sh
+ln -s $PWD/polybar_config ~/.config/polybar/config
