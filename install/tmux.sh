3aac60ad5e92f943b61c48d049c8651c6304aa0c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-07 08:39:15 +0100

    reorder steps in tmux installation to make it easier to read

--- install/tmux.sh
+++ install/tmux.sh
@@ -5,6 +5,10 @@ CONFIGS_DIR=$(dirname $PWD)/configs
 sudo apt install tmux
 ln -s $CONFIGS_DIR/tmux.conf ~/.tmux.conf
 
+echo "Installing powerline for tmux bottom bar to work correctly"
+sudo apt install powerline
+echo "Done"
+
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Do 'Ctrl + b I' in tmux to install the plugins"
 
@@ -13,6 +17,3 @@ source ./bash-completions-dir.sh
 COMPLETIONS_PATH=$BASH_COMPLETIONS_DIR/tmux.bash
 wget -qO $COMPLETIONS_PATH https://raw.githubusercontent.com/imomaliev/tmux-bash-completion/master/completions/tmux
 echo "Completions saved to $COMPLETIONS_PATH " 
-
-echo "Installing powerline for tmux bottom bar to work correctly"
-sudo apt install powerline
