883141343d867bfdcf11d8412d6f6fc21addd97e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-07 09:10:18 +0100

    fix instructions for tmux font installation

--- install/tmux.sh
+++ install/tmux.sh
@@ -7,8 +7,14 @@ ln -s $CONFIGS_DIR/tmux.conf ~/.tmux.conf
 
 echo "For tmux bottom bar to work correctly, powerline icons are required"
 if grep -qi Microsoft /proc/version; then
-	echo "Looks like you're on WSL, install the Cascadia Code font for powerline symbols"
-	echo "Go to https://github.com/microsoft/cascadia-code/releaes"
+	echo "Looks like you're on WSL"
+	echo "1. Install a font that has powerline symbols"
+	echo "   Choose one from https://www.nerdfonts.com/font-downloads"
+	echo "   e.g. FiraCode NF"
+	echo "2. Select that font as the default font in your terminal"
+	echo "   e.g. in Windows Terminal, set fontFamily: FiraCode NF"
+	echo "3. (optional) Adjust fontSize, as it probably is a bit too large"
+	echo "   11 should be ok"
 else
 	echo "Detected regular system (not WSL), installing through apt"
 	sudo apt install powerline
