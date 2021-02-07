65c8665424683caea13af1c49317454cb31167c6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-07 08:52:51 +0100

    add powerline installation instructions for WSL

--- install/tmux.sh
+++ install/tmux.sh
@@ -5,9 +5,15 @@ CONFIGS_DIR=$(dirname $PWD)/configs
 sudo apt install tmux
 ln -s $CONFIGS_DIR/tmux.conf ~/.tmux.conf
 
-echo "Installing powerline for tmux bottom bar to work correctly"
-sudo apt install powerline
-echo "Done"
+echo "For tmux bottom bar to work correctly, powerline icons are required"
+if grep -qi Microsoft /proc/version; then
+	echo "Looks like you're on WSL, install the Cascadia Code font for powerline symbols"
+	echo "Go to https://github.com/microsoft/cascadia-code/releaes"
+else
+	echo "Detected regular system (not WSL), installing through apt"
+	sudo apt install powerline
+	echo "Done"
+fi
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 echo "Do 'Ctrl + b I' in tmux to install the plugins"
