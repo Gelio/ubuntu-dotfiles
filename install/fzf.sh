02a4a41e0d6f5cf0f65519674ff43ee141c2b6b1
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-09 08:43:58 +0100

    add info on how to enable fzf keybindings and completions

--- install/fzf.sh
+++ install/fzf.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
 sudo apt install fzf
+
+echo "If you are on Ubuntu and keybindings (e.g. Ctrl-T) are not working, see 'apt-cache show fzf'"
+echo "You may have to add some lines to ~/.bashrc manually"
