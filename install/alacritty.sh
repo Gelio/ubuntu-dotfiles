8fec8543e7fa62cc0182c44d16a09b93608116eb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-22 09:20:46 +0100

    add a way to only update alacritty completions

--- install/alacritty.sh
+++ install/alacritty.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-sudo add-apt-repository ppa:aslatter/ppa
-sudo apt install alacritty
+# https://stackoverflow.com/a/13864829
+if [ -z "${COMPLETIONS_ONLY+x}" ]; then
+    sudo add-apt-repository ppa:aslatter/ppa
+    sudo apt install alacritty
 
-CONFIGS_DIR=$(dirname $PWD)/configs
+    CONFIGS_DIR=$(dirname $PWD)/configs
 
-mkdir -p ~/.config/alacritty
-ln -s "$CONFIGS_DIR/alacritty.yml" ~/.config/alacritty/alacritty.yml
+    mkdir -p ~/.config/alacritty
+    ln -s "$CONFIGS_DIR/alacritty.yml" ~/.config/alacritty/alacritty.yml
 
-echo "If you're on Ubuntu, add 'wmctrl -a alacritty' keyboard shortcut"
+    echo "If you're on Ubuntu, add 'wmctrl -a alacritty' keyboard shortcut"
+fi
 
 echo "Adding bash completions for the latest release"
 source ./bash-completions-dir.sh
