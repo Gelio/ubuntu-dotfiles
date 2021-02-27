00e000b1a3d34711c3499a4db334fd32a52bc948
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-02-27 10:05:57 +0100

    add bit installation script

---
+++ install/bit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# https://github.com/chriswalz/bit
+# An alternative client for git
+curl -sf https://gobinaries.com/chriswalz/bit | sh;
+bit complete;
+echo "Type bit then press <ENTER> to show interactive prompt"
