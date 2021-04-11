6f5b38464e7d57c58a1d32b19e6570fb27b76348
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-04-11 17:40:03 +0200

    use git directly when installing fzf

--- install/fzf.sh
+++ install/fzf.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo apt install fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
 
 echo "If you are on Ubuntu and keybindings (e.g. Ctrl-T) are not working, see 'apt-cache show fzf'"
 echo "You may have to add some lines to ~/.bashrc manually"
