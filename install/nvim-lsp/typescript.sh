36c111695cf7536fc4682c0c8990dc1d7b53eddc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-04-22 09:25:53 +0200

    add automatic updating of efm-langserver

--- install/nvim-lsp/typescript.sh
+++ install/nvim-lsp/typescript.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 
 npm install -g eslint_d typescript-language-server @fsouza/prettierd vscode-json-languageserver
 # efm-langserver to run daemons (eslint and prettier)
-go get github.com/mattn/efm-langserver
+go get -u github.com/mattn/efm-langserver
