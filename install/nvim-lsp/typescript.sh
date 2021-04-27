0728bc08693a0364de705d653fa8e8ad2f9da860
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-04-27 09:19:13 +0200

    mention installing efm-langserver@master

--- install/nvim-lsp/typescript.sh
+++ install/nvim-lsp/typescript.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 npm install -g eslint_d typescript-language-server @fsouza/prettierd vscode-json-languageserver
 # efm-langserver to run daemons (eslint and prettier)
 go get -u github.com/mattn/efm-langserver
+echo 'You may have to reinstall efm-langserver@master'
