8d9bf2b028e0be6773152b91697570c77dc4651f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-05-22 14:06:38 +0200

    add graphql LSP

--- configs/neovim.lua
+++ configs/neovim.lua
@@ -53,7 +53,7 @@ local on_attach = function(client, bufnr)
   require'lsp_signature'.on_attach()
 end
 
-local servers_with_defaults = { "gopls", "rust_analyzer", "jsonls", "bashls", "cssls", "svelte" }
+local servers_with_defaults = { "gopls", "rust_analyzer", "jsonls", "bashls", "cssls", "svelte", "graphql" }
 for _, lsp in ipairs(servers_with_defaults) do
   nvim_lsp[lsp].setup { on_attach = on_attach }
 end
---
+++ install/nvim-lsp/graphql.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+npm i -g graphql-language-service-cli
