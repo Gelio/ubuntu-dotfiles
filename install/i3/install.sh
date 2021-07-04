2d43fe5364a593b2aa157ebaf9a060b5e88558fc
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-07-04 12:58:09 +0200

    add autolock on i3

--- install/i3/install.sh
+++ install/i3/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 I3_ADDITIONAL_PACKAGES=lxappearance picom hsetroot rofi xsettingsd \
 	fonts-font-awesome numlockx xfce4-power-manager dunst redshift mpd
-SCREENSAVER_PACKAGES=xscreensaver xscreensaver-gl-extra
+SCREENSAVER_PACKAGES=xscreensaver xscreensaver-gl-extra xautolock
 ROFIMOJI_PACKAGES=python3 python3-pip xdotool
 
 sudo add-apt-repository ppa:regolith-linux/release
