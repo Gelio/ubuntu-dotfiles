bd255a03ad23da10eccbf7b100055cc5d8933d91
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-07-04 21:08:34 +0200

    add keyboard media controls to i3

--- install/i3/install.sh
+++ install/i3/install.sh
@@ -2,7 +2,8 @@
 set -euxo pipefail
 
 I3_ADDITIONAL_PACKAGES=lxappearance picom hsetroot rofi xsettingsd \
-	fonts-font-awesome numlockx xfce4-power-manager dunst redshift mpd
+	fonts-font-awesome numlockx xfce4-power-manager dunst redshift mpd \
+  playerctl
 SCREENSAVER_PACKAGES=xscreensaver xscreensaver-gl-extra xautolock
 ROFIMOJI_PACKAGES=python3 python3-pip xdotool
 
