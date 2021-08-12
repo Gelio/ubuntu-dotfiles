16154a4276ede4cd19c9a7cf323cbc27c64fe54d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-12 08:55:27 +0200

    Add misspell LSP diagnostics in neovim
    Along with an install script

---
+++ install/neovim/lsp-servers/misspell.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+go get -u github.com/client9/misspell/cmd/misspell
--- install/neovim/stowed/lua/lsp/init.lua
+++ install/neovim/stowed/lua/lsp/init.lua
@@ -118,6 +118,7 @@ local null_ls_sources = {
 	null_ls.builtins.diagnostics.hadolint,
 	null_ls.builtins.diagnostics.markdownlint,
 	null_ls.builtins.diagnostics.write_good,
+	null_ls.builtins.diagnostics.misspell,
 }
 null_ls.config({
 	sources = null_ls_sources,
