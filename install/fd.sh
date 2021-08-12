357670148ae04eef391a4cfee9a7ec23370b7cb6
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-12 19:37:55 +0200

    change fd alias to a symlink
    Bash aliases do not work in Makefiles and in `watch` command.
    It is safer to use a symlink

--- install/fd.sh
+++ install/fd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install fd-find
-echo "alias fd=fdfind" >> ~/.bash_aliases
+ln -s "$(which fdfind)" ~/.local/bin/fd
 
 printf "Run: source ~/.bash_aliases\nto be able to run fd\n"
