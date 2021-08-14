94769ca77f8442ef31d488c5ca55671c47db34b8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-14 17:42:49 +0200

    add removing the regular i3lock when installing i3lock-color
    Fixes manpages

--- install/i3/install-i3lock-color.sh
+++ install/i3/install-i3lock-color.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 
 # https://github.com/Raymo111/i3lock-color
+# Remove the default i3lock as i3lock-color uses the same binary name
+# Otherwise, e.g. man pages will not be installed
+sudo apt remove i3lock
 
 # https://github.com/Raymo111/i3lock-color#ubuntu-182004-lts
 sudo apt install autoconf gcc make pkg-config libpam0g-dev libcairo2-dev libfontconfig1-dev libxcb-composite0-dev libev-dev libx11-xcb-dev libxcb-xkb-dev libxcb-xinerama0-dev libxcb-randr0-dev libxcb-image0-dev libxcb-util-dev libxcb-xrm-dev libxcb-xtest0-dev libxkbcommon-dev libxkbcommon-x11-dev libjpeg-dev
