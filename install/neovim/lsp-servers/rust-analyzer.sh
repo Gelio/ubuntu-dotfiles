74325bcaa48d2a34180a122dc4e3d37e2c547bcb
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-14 07:46:20 +0200

    fix rust-analyzer URL

--- install/neovim/lsp-servers/rust-analyzer.sh
+++ install/neovim/lsp-servers/rust-analyzer.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-RUST_ANALYZER_URL="https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux.gz"
+RUST_ANALYZER_URL="https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz"
 
 wget $RUST_ANALYZER_URL -O /tmp/rust-analyzer.gz
 gunzip /tmp/rust-analyzer.gz
