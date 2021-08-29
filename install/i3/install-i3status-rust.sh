4c88824667bf191ae7468cb169063d218f16e93c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-29 15:02:54 +0200

    add missing libdbus-1-dev dependency of i3status-rust

--- install/i3/install-i3status-rust.sh
+++ install/i3/install-i3status-rust.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+# https://github.com/greshake/i3status-rust/issues/194
+sudo apt install libdbus-1-dev
+
 cd ~/.local
 git clone https://github.com/greshake/i3status-rust
 cd i3status-rust
