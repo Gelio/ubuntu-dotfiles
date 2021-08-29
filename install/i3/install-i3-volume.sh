c7387a2bcbcfb08f4b88da299aa51ae606c4f419
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-29 17:08:55 +0200

    add custom volume control script
    Uses dunst progress bar instead of a custom one, which looks better.

--- install/i3/install-i3-volume.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -eou pipefail
-
-cd ~/.local
-[[ ! -d i3-volume ]] && git clone git@github.com:hastinbe/i3-volume.git
-ln -s "$PWD/volume" ~/.local/bin/volume
--- install/i3/install.sh
+++ install/i3/install.sh
@@ -3,7 +3,7 @@ set -euxo pipefail
 
 I3_ADDITIONAL_PACKAGES="lxappearance hsetroot rofi xsettingsd \
   fonts-font-awesome numlockx xfce4-power-manager \
-  mpd playerctl xfce4-volumed autorandr feh"
+  mpd playerctl xfce4-volumed autorandr feh pavucontrol"
 ROFIMOJI_PACKAGES="python3 python3-pip xdotool"
 LOCKSCREEN_PACKAGES="libxcb-screensaver0 libxcb-screensaver0-dev"
 
@@ -33,7 +33,6 @@ echo "See https://github.com/phillipberndt/autorandr"
 ./install-picom.sh
 ./install-libinput-gestures.sh
 ./install-dunst.sh
-./install-i3-volume.sh
 ./install-i3lock-color.sh
 ./install-betterlockscreen.sh
 ./install-redshift.sh
