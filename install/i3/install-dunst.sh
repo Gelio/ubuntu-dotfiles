f78a7517fa0babdff67c12862100e5499f3750db
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-29 15:02:33 +0200

    remove default dunst package before building it from source

--- install/i3/install-dunst.sh
+++ install/i3/install-dunst.sh
@@ -2,6 +2,9 @@
 
 set -euo pipefail
 
+# Remove dunst that is installed by default with i3
+sudo apt remove dunst
+
 # https://github.com/dunst-project/dunst/wiki/Dependencies#ubuntu
 sudo apt install libdbus-1-dev libx11-dev libxinerama-dev libxrandr-dev libxss-dev libglib2.0-dev \
   libpango1.0-dev libgtk-3-dev libxdg-basedir-dev libnotify-dev
