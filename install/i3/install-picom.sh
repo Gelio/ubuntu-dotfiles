34ea63b8ce3d437a206975888cd6c5d78dad3de5
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-29 15:03:11 +0200

    add picom install instructions
    Use the instructions from https://github.com/yshui/picom#build

--- install/i3/install-picom.sh
+++ install/i3/install-picom.sh
@@ -2,6 +2,20 @@
 
 set -euo pipefail
 
-echo "Go to https://github.com/yshui/picom#build and follow the instructions"
+# https://github.com/yshui/picom#build
 
-echo 'Then, execute: ls -s "$PWD/build/src/picom" ~/.local/bin'
+sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev \
+  libxcb-xfixes0-dev libxcb-shape0-dev libxcb-render-util0-dev \
+  libxcb-render0-dev libxcb-randr0-dev libxcb-composite0-dev \
+  libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev \
+  libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev \
+  libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev \
+  uthash-dev libev-dev libx11-xcb-dev meson
+
+cd ~/.local
+[[ ! -d picom ]] && git clone git@github.com:yshui/picom.git
+cd picom
+git submodule update --init --recursive
+meson --buildtype=release . build
+ninja -C build
+ln -s "$PWD/build/src/picom" ~/.local/bin
