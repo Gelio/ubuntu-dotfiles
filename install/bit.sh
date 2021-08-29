e9351ff3587e88431c9b46feaa33dfa8ea49c24f
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-08-29 14:43:09 +0200

    minor improvements to existing install scripts

--- install/bit.sh
+++ install/bit.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+set -euo pipefail
+
 # https://github.com/chriswalz/bit
 # An alternative client for git
-curl -sf https://gobinaries.com/chriswalz/bit | sh;
-bit complete;
+curl -sf https://gobinaries.com/chriswalz/bit | sh
+bit complete
 echo "Type bit then press <ENTER> to show interactive prompt"
--- install/node.sh
+++ install/node.sh
@@ -1,11 +1,14 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
-nvm install --lts
-nvm use --lts
+set -euo pipefail
+
+if ! command -v node &>/dev/null; then
+  echo 'Run "nvm use --lts" to install node and npm'
+  exit 1
+fi
 
 echo "Adding completions"
 source ./bash-completions-dir.sh
-NPM_BASH_COMPLETIONS_PATH=$BASH_COMPLETIONS_DIR/npm.bash
-npm completion > $NPM_BASH_COMPLETIONS_PATH
-echo "Completions saved to $NPM_BASH_COMPLETIONS_PATH" 
+NPM_BASH_COMPLETIONS_PATH="$BASH_COMPLETIONS_DIR/npm.bash"
+npm completion >$NPM_BASH_COMPLETIONS_PATH
+echo "Completions saved to $NPM_BASH_COMPLETIONS_PATH"
