5052a14b1f185de464e7ae48b52eed66efd98b11
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-07 11:58:21 +0200

    fix configuring JSON completion for some files and add new schemas
    Turns out the previous way of adding known schemas was wrong.
    
    This commit also adds schemas for cypress.json and .prettierrc.

--- install/neovim/stowed/.config/nvim/lua/lsp/init.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/init.lua
@@ -109,39 +109,36 @@ nvim_lsp.jsonls.setup({
 	settings = {
 		json = {
 			schemas = {
-				-- https://github.com/microsoft/vscode/blob/6e5ffbde32cb783be15ddd383028c4a56a667edd/extensions/typescript-language-features/package.json#L70
-				-- Use http://mageddo.com/tools/json-to-lua-converter
+				-- Sources
+				-- 1. VSCode
+				--    https://github.com/microsoft/vscode/blob/6e5ffbde32cb783be15ddd383028c4a56a667edd/extensions/typescript-language-features/package.json#L70
+				--    Use http://mageddo.com/tools/json-to-lua-converter
+				--
+				-- 2. SchemaStore
+				--    https://www.schemastore.org/json/
 				{
-					fileMatch = "tsconfig.json",
+					fileMatch = { "package.json" },
+					url = "https://json.schemastore.org/package",
+				},
+				{
+					fileMatch = { "tsconfig.json", "tsconfig-*.json", "tsconfig.*.json" },
 					url = "https://json.schemastore.org/tsconfig",
 				},
 				{
-					fileMatch = "tsconfig.*.json",
-					url = "https://json.schemastore.org/tsconfig",
-				},
-				{
-					fileMatch = "tsconfig-*.json",
-					url = "https://json.schemastore.org/tsconfig",
-				},
-				{
-					fileMatch = ".babelrc",
+					fileMatch = { ".babelrc", ".babelrc.json", "babel.config.json" },
 					url = "https://json.schemastore.org/babelrc",
 				},
 				{
-					fileMatch = ".babelrc.json",
-					url = "https://json.schemastore.org/babelrc",
-				},
-				{
-					fileMatch = "babel.config.json",
-					url = "https://json.schemastore.org/babelrc",
-				},
-				{
-					fileMatch = "jsconfig.json",
+					fileMatch = { "jsconfig.json", "jsconfig.*.json" },
 					url = "https://json.schemastore.org/jsconfig",
 				},
 				{
-					fileMatch = "jsconfig.*.json",
-					url = "https://json.schemastore.org/jsconfig",
+					fileMatch = { ".prettierrc", ".prettierrc.json" },
+					url = "https://json.schemastore.org/prettierrc.json",
+				},
+				{
+					fileMatch = { "cypress.json" },
+					url = "https://raw.githubusercontent.com/cypress-io/cypress/develop/cli/schema/cypress.schema.json",
 				},
 			},
 		},
