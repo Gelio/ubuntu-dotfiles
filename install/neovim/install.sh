978f6caa860dc59734938887333f469069033278
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-08 09:43:16 +0200

    switch neovim from appimage to a regular binary
    Allows using nvim as MANPAGER
    Adds a man page for nvim

--- install/neovim/install.sh
+++ install/neovim/install.sh
@@ -1,19 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-mkdir -p ~/.config
-wget -O ~/.config/nvim.appimage https://github.com/neovim/neovim/releases/download/${NVIM_VERSION-=default}/nvim.appimage
-
+nvim_version=${1:-nightly}
+directory_name="nvim-linux64"
+archive_name="$directory_name.tar.gz"
 upgrade_only=$(command -v nvim || true)
 
+echo "* Downloading $nvim_version"
+
+wget "https://github.com/neovim/neovim/releases/download/$nvim_version/$archive_name"
+tar -xzf "$archive_name"
+rm "$archive_name"
+
+echo "* Installing via stow"
+sudo stow -R --no-folding -t /usr $directory_name
+
+nvim -v
+
 if [ -n "$upgrade_only" ]; then
-  nvim -v
   echo "Upgraded neovim, exiting"
   exit 0
 fi
-
-chmod u+x ~/.config/nvim.appimage
-sudo ln -s "$HOME/.config/nvim.appimage" /usr/bin/nvim
 
 ./stow.sh
 
