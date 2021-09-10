fbc135324b0817165991be3bcf54e5dd1587d6ca
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-10 09:23:41 +0200

    add removing stowed files when upgrading neovim
    Prevents possibly leaving behind broken symlinks

--- install/neovim/install.sh
+++ install/neovim/install.sh
@@ -6,28 +6,41 @@ directory_name="nvim-linux64"
 archive_name="$directory_name.tar.gz"
 upgrade_only=$(command -v nvim || true)
 
-echo "* Downloading $nvim_version"
+function main {
+  echo "* Downloading $nvim_version"
+  wget "https://github.com/neovim/neovim/releases/download/$nvim_version/$archive_name"
 
-wget "https://github.com/neovim/neovim/releases/download/$nvim_version/$archive_name"
-tar -xzf "$archive_name"
-rm "$archive_name"
+  stow_package -D
 
-echo "* Installing via stow"
-sudo stow -R --no-folding -t /usr $directory_name
+  tar -xzf "$archive_name"
+  rm "$archive_name"
 
-nvim -v
+  echo "* Installing via stow"
+  stow_package
 
-if [ -n "$upgrade_only" ]; then
-  echo "Upgraded neovim, exiting"
-  exit 0
-fi
+  nvim -v
 
-./stow.sh
+  if [ -n "$upgrade_only" ]; then
+    echo "Upgraded neovim, exiting"
+    exit 0
+  fi
 
-# Install python3 provider for neovim
-# See https://neovim.io/doc/user/provider.html
-python3 -m pip install --user --upgrade pynvim
+}
 
-# xsel for clipboard support
-# g++ for compiling Treesitter parsers
-sudo apt install xsel g++
+function install {
+  ./stow.sh
+
+  # Install python3 provider for neovim
+  # See https://neovim.io/doc/user/provider.html
+  python3 -m pip install --user --upgrade pynvim
+
+  # xsel for clipboard support
+  # g++ for compiling Treesitter parsers
+  sudo apt install xsel g++
+}
+
+function stow_package {
+  sudo stow --no-folding -t /usr "$@" $directory_name
+}
+
+main
