d3c815cd2e9d6ba5aa5c95778b5c3622659e5728
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-09-16 09:34:22 +0200

    switch meson to install from pip
    Uses most up-to-date version.
    The version in apt was outdated (0.53 vs 0.59)

--- install/i3/install-picom.sh
+++ install/i3/install-picom.sh
@@ -10,7 +10,8 @@ sudo apt install libxext-dev libxcb1-dev libxcb-damage0-dev \
   libxcb-image0-dev libxcb-present-dev libxcb-xinerama0-dev \
   libxcb-glx0-dev libpixman-1-dev libdbus-1-dev libconfig-dev \
   libgl1-mesa-dev libpcre2-dev libpcre3-dev libevdev-dev \
-  uthash-dev libev-dev libx11-xcb-dev meson
+  uthash-dev libev-dev libx11-xcb-dev
+pip3 install --user meson
 
 cd ~/.local
 [[ ! -d picom ]] && git clone git@github.com:yshui/picom.git
