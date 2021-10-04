cdbfc95d78656c05754bd2469e29492e4c8c84ed
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-10-04 21:45:40 +0200

    Use plenary for auto-nvimrc (WIP) (scratch this commit)

--- install/neovim/stowed/.config/nvim/lua/auto-nvimrc/init.lua
+++ install/neovim/stowed/.config/nvim/lua/auto-nvimrc/init.lua
@@ -19,6 +19,46 @@ local M = {}
 -- Use plenary.log for logging
 -- https://github.com/nvim-lua/plenary.nvim/blob/master/lua/plenary/log.lua
 
+local sha = require("auto-nvimrc.sha2")
+local async = require("plenary.async")
+local fun_utils = require("plenary.fun")
+
+local function read_file(file_path)
+	-- TODO: improve error handling
+	-- 438 = 0o444 (mode)
+	local _err, fd = async.uv.fs_open(file_path, "r", 438)
+	assert(not _err, _err)
+
+	local _err, stat = async.uv.fs_fstat(fd)
+	assert(not _err, _err)
+
+	local _err, contents = async.uv.fs_read(fd, stat.size, 0)
+	assert(not _err, _err)
+
+	local _err = async.uv.fs_close(fd)
+	assert(not _err, _err)
+
+	return contents
+end
+
+local function get_file_hash(file_path)
+	return sha.sha256(read_file(file_path))
+end
+
+local function head(tbl)
+	return tbl[1]
+end
+
+local function get_file_hashes(file_paths)
+	local wrapped_hashes = async.util.join(vim.tbl_map(function(path)
+		return function()
+			return get_file_hash(path)
+		end
+	end, file_paths))
+
+	return vim.tbl_map(head, wrapped_hashes)
+end
+
 function M.execute_nvimrcs()
 	local nvimrcs = vim.fn.findfile(".nvimrc", ".;", -1)
 
@@ -26,17 +66,27 @@ function M.execute_nvimrcs()
 		return
 	end
 
+	local nvimrcs_absolute_paths = vim.tbl_map(function(path)
+		return vim.fn.fnamemodify(path, ":p")
+	end, nvimrcs)
+
+	async.run(function()
+		return get_file_hashes(nvimrcs_absolute_paths)
+	end, function(hashes)
+		print("got hashes", vim.inspect(hashes))
+	end)
+
 	-- TODO: use plenary.path module for reading files
 
 	-- Loop from outermost (least specific) to innermost (most specific)
 	for i = #nvimrcs, 1, -1 do
-		local full_path = vim.fn.fnamemodify(nvimrcs[i], ":p")
+		local absolute_path = vim.fn.fnamemodify(nvimrcs[i], ":p")
 
-		print("Source " .. full_path .. "? [y]/n: ")
+		print("Source " .. absolute_path .. "? [y]/n: ")
 		local choice = vim.fn.getcharstr()
 		if string.lower(choice) ~= "n" then
-			print("Sourcing " .. full_path)
-			vim.cmd("source " .. full_path)
+			print("Sourcing " .. absolute_path)
+			vim.cmd("source " .. absolute_path)
 		end
 	end
 end
