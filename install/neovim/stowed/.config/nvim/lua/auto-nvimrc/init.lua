1ebb7d3b599622dafea2b735c9303745d7163846
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-10-05 20:34:48 +0200

    Add filtering read files and comparing hashes (WIP)
    It turned out to be a mess. Needs a rewrite, but some parts are already
    functional.

--- install/neovim/stowed/.config/nvim/lua/auto-nvimrc/init.lua
+++ install/neovim/stowed/.config/nvim/lua/auto-nvimrc/init.lua
@@ -22,6 +22,7 @@ local M = {}
 local sha = require("auto-nvimrc.sha2")
 local async = require("plenary.async")
 local Path = require("plenary.path")
+local func = require("plenary.functional")
 
 ---Composes two functions
 ---@generic A
@@ -61,34 +62,135 @@ local function defer_call(fn)
 	end
 end
 
----@param file_paths string[]
+-- TODO:
+-- 1. Use vim.json.decode and vim.json.encode (for performance gains)
+-- 2. Operate on objects (use OOP instead of procedural programming)
+
+---@alias nvimrc_exec_status '"always"' | '"always_until_modified"' | '"ask"' | '"never"' | '"never_until_modified"'
+
+---@type table<nvimrc_exec_status, nvimrc_exec_status>
+local nvimrc_states = vim.tbl_add_reverse_lookup({
+	"always",
+	"always_until_modified",
+	"ask",
+	"never",
+	"never_until_modified",
+})
+
+---@param file_paths "Path[]"
 ---@return string[]
 local function get_file_hashes(file_paths)
-	local wrapped_hashes = async.util.join(vim.tbl_map(defer_call(compose(get_file_hash, Path.new)), file_paths))
+	local wrapped_hashes = async.util.join(vim.tbl_map(defer_call(get_file_hash), file_paths))
 
 	return vim.tbl_map(head, wrapped_hashes)
 end
 
-local function run(nvimrcs_absolute_paths)
+---@class NvimrcInfo
+---@field hash string
+---@field status nvimrc_exec_status
+
+---@alias NvimrcConfig table<string, NvimrcInfo>
+
+---@param path "Path"
+---@return NvimrcConfig
+local function get_nvimrc_config_from_path(path)
+	local contents = read_file(path)
+	async.util.scheduler()
+
+	if string.len(contents) == 0 then
+		return {}
+	else
+		return vim.fn.json_decode(contents)
+	end
+end
+
+---@type nvimrc_exec_status[]
+local statuses_to_never_read = { "always", "never" }
+---Checks if a given nvimrc file should be read.
+---Files that are always ignored or always accepted
+---should never be read.
+---@param config NvimrcConfig
+---@param path "Path"
+local function should_read_file(config, path)
+	local nvimrc_info = config[path:absolute()]
+	if nvimrc_info == nil then
+		return true
+	end
+
+	return not vim.tbl_contains(statuses_to_never_read, nvimrc_info.status)
+end
+
+---@alias ask_reason "new_file" | "exec_status_ask" | "modified"
+
+---Decides what to do about an nvimrc file.
+---@param config NvimrcConfig
+---@param paths "Path[]"
+---@param hashes string[]
+---@returns table List of nvimrcs to ask whether to execute
+local function get_files_to_ask(config, paths, hashes)
+	local paths_to_ask = {}
+
+	for i, path in ipairs(paths) do
+		local nvimrc_info = config[path:absolute()]
+		---@type ask_reason
+		local reason
+
+		if nvimrc_info == nil then
+			reason = "new_file"
+		elseif nvimrc_info.status == "ask" then
+			reason = "exec_status_ask"
+		else
+			local hash = hashes[i]
+			if nvimrc_info.hash ~= hash then
+				reason = "modified"
+			end
+		end
+
+		if reason ~= nil then
+			table.insert(paths_to_ask, { path = path, reason = reason })
+		end
+	end
+
+	return paths_to_ask
+end
+
+local function ask_for_files_execution(config, files_to_ask)
+	async.util.scheduler()
+	for _, file_to_ask in ipairs(files_to_ask) do
+		local choice = vim.fn.inputlist({
+			"File " .. file_to_ask.path:absolute() .. " is " .. file_to_ask.reason .. ". Execute?",
+			"1. Yes, always",
+			"2. Yes, always until changed",
+			"3. Yes, but ask again next time",
+			"4. No, but ask again next time",
+			"5. No, never",
+			"6. No, never until changed",
+		})
+		print("\n")
+	end
+end
+
+local function run(nvimrcs)
+	local nvimrcs_absolute_paths = vim.tbl_map(Path.new, nvimrcs)
+
 	local config_path = Path.new(vim.fn.stdpath("data"), "auto-nvimrc", "config.json")
+	-- NOTE: Creates config file and parent directories in case they do not exist
 	config_path:touch({ parents = true })
 
 	async.run(function()
-		local config_contents = read_file(config_path)
-		print("contents", config_contents)
-		async.util.scheduler()
-		print(vim.inspect(vim.fn.json_decode(config_contents)))
-		local hashes = get_file_hashes(nvimrcs_absolute_paths)
-		local new_config = {}
+		local config = get_nvimrc_config_from_path(config_path)
+		local paths_to_load = vim.tbl_filter(func.partial(should_read_file, config), nvimrcs_absolute_paths)
 
-		for i, hash in ipairs(hashes) do
-			new_config[nvimrcs_absolute_paths[i]] = hash
-		end
+		-- 3. Ask if files should be executed
+		-- 4. Save the results
+		local hashes = get_file_hashes(paths_to_load)
+		local paths_to_ask = get_files_to_ask(config, paths_to_load, hashes)
+		ask_for_files_execution(config, paths_to_ask)
 
-		async.util.scheduler()
-		local serialized_config = vim.fn.json_encode(new_config)
-		print(serialized_config)
-		config_path:write(serialized_config, "w")
+		-- async.util.scheduler()
+		-- local serialized_config = vim.fn.json_encode(new_config)
+		-- print(serialized_config)
+		-- config_path:write(serialized_config, "w")
 
 		return hashes
 	end, function(hashes)
@@ -103,11 +205,7 @@ function M.execute_nvimrcs()
 		return
 	end
 
-	local nvimrcs_absolute_paths = vim.tbl_map(function(path)
-		return vim.fn.fnamemodify(path, ":p")
-	end, nvimrcs)
-
-	run(nvimrcs_absolute_paths)
+	run(nvimrcs)
 
 	-- Loop from outermost (least specific) to innermost (most specific)
 	for i = #nvimrcs, 1, -1 do
