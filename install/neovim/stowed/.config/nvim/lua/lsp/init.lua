8a39aee019f0cb2f65083053b8962ee6b8b99a63
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-10-25 10:01:17 +0200

    Add borders to hover and signature-help popups
    As an experiment. The commit may be reverted in the future if I do not
    like it.

--- install/neovim/stowed/.config/nvim/lua/lsp/init.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/init.lua
@@ -30,5 +30,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 		source = "always",
 	},
 })
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 
 require("auto-nvimrc").execute_nvimrcs()
