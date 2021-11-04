e1731ae1500c6dba732def5ba91b1c6fc218b925
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-04 10:42:59 +0000

    refactor(plugins): update the pre_hook for Comment.nvim
    Use the updated suggested integration of Comment.nvim with
    nvim-ts-context-commentstring.
    
    See https://github.com/numToStr/Comment.nvim#-hooks for more details.

--- install/neovim/stowed/.config/nvim/lua/plugins.lua
+++ install/neovim/stowed/.config/nvim/lua/plugins.lua
@@ -200,8 +200,26 @@ local function setup_packer(packer_bootstrap)
 			"numToStr/Comment.nvim",
 			config = function()
 				require("Comment").setup({
-					pre_hook = function()
-						return require("ts_context_commentstring.internal").calculate_commentstring()
+					pre_hook = function(ctx)
+						if vim.bo.filetype == "typescriptreact" then
+							local U = require("Comment.utils")
+
+							-- Detemine whether to use linewise or blockwise commentstring
+							local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+
+							-- Determine the location where to calculate commentstring from
+							local location = nil
+							if ctx.ctype == U.ctype.block then
+								location = require("ts_context_commentstring.utils").get_cursor_location()
+							elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
+								location = require("ts_context_commentstring.utils").get_visual_start_location()
+							end
+
+							return require("ts_context_commentstring.internal").calculate_commentstring({
+								key = type,
+								location = location,
+							})
+						end
 					end,
 				})
 			end,
