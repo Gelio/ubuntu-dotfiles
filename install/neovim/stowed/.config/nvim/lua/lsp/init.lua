6ec7b6fdd2717ad7a1e49947258402401efbdd5a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-09 08:21:01 +0100

    feat(lsp): install vim language server

--- install/neovim/stowed/.config/nvim/lua/lsp/init.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/init.lua
@@ -24,6 +24,7 @@ nvim_lsp.tsserver.setup(require("lsp.tsserver").config)
 nvim_lsp.sumneko_lua.setup(require("lsp.lua").config)
 
 nvim_lsp.yamlls.setup(utils.base_config)
+nvim_lsp.vimls.setup(utils.base_config)
 
 require("lsp.java").setup()
 
