4994dc9118ffd7bf3bb1c44b45e9d42f074b4058
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-12 09:23:16 +0100

    chore(nvim): add plenary.nvim as a dependency of diffview.nvim

--- install/neovim/stowed/.config/nvim/lua/plugins.lua
+++ install/neovim/stowed/.config/nvim/lua/plugins.lua
@@ -698,6 +698,7 @@ local function setup_packer(packer_bootstrap)
 
 		use({
 			"sindrets/diffview.nvim",
+			requires = { "nvim-lua/plenary.nvim" },
 			config = function()
 				require("diffview").setup({})
 			end,
