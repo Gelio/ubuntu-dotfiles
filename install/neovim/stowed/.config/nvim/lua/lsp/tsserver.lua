9fb27704caf1f16ab14f87a613f39d6789222474
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-18 08:41:09 +0100

    refactor(ts-lsp): remove deprecated nvim-lsp-ts-utils config
    ESLint and prettier support are deprecated in nvim-lsp-ts-utils. I was
    not using them anyway, so it makes sense to remove them.
    
    See https://github.com/jose-elias-alvarez/nvim-lsp-ts-utils/issues/87

--- install/neovim/stowed/.config/nvim/lua/lsp/tsserver.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/tsserver.lua
@@ -2,11 +2,7 @@ local M = {}
 
 local function setup_lsp_ts_utils(client)
 	local ts_utils = require("nvim-lsp-ts-utils")
-	ts_utils.setup({
-		-- ESLint integration provided by ESLint language server
-		eslint_enable_code_actions = false,
-		eslint_enable_diagnostics = false,
-	})
+	ts_utils.setup({})
 	ts_utils.setup_client(client)
 end
 
