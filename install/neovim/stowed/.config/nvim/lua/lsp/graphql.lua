fe899c9c7a8629ce5c578044079f2982859cf974
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-11-30 09:58:18 +0100

    fix(nvim): stop attaching graphql LS to every TS file
    GraphQL LS was being attached to every TS file. This caused messages to
    be shown in the command-line whenever GraphQL was not configured in a
    repository, which started to become annoying.

--- install/neovim/stowed/.config/nvim/lua/lsp/graphql.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/graphql.lua
@@ -1,8 +1,5 @@
 local M = {}
 
-M.config = vim.tbl_extend("error", require("lsp.utils").base_config, {
-	root_dir = require("lspconfig").util.root_pattern(".graphqlrc*", ".git"),
-	filetypes = { "graphql", "typescript", "typescriptreact" },
-})
+M.config = require("lsp.utils").base_config
 
 return M
