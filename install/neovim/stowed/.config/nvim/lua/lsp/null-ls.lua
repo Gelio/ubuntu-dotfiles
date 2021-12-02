26966d682e09e58393518f08de1dccb08fc9e15b
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-12-02 10:37:46 +0100

    feat(nvim): add shellcheck code actions

--- install/neovim/stowed/.config/nvim/lua/lsp/null-ls.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/null-ls.lua
@@ -16,6 +16,7 @@ M.sources = {
 	null_ls.builtins.formatting.stylua,
 	null_ls.builtins.diagnostics.selene,
 	null_ls.builtins.diagnostics.shellcheck,
+	null_ls.builtins.code_actions.shellcheck,
 	null_ls.builtins.formatting.shfmt,
 	null_ls.builtins.diagnostics.hadolint,
 	null_ls.builtins.diagnostics.markdownlint,
