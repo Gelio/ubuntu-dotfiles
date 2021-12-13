8d9d1f00ba42bc9b53a0fb6f123eefa51e85dc54
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2021-12-13 06:35:23 +0100

    fix(i3): enabling lock screen service on boot
    betterlockscreen service could not be enabled on boot because the
    symlinked service definition could not be found. This is possible
    because it was a symlink to the user's home folder, which may not have
    been loaded at the time of systemd initialization.
    
    Copying the service instead of symlinking it seems to have solved the
    problem.

--- install/i3/install-betterlockscreen.sh
+++ install/i3/install-betterlockscreen.sh
@@ -8,6 +8,11 @@ VERSION="v4.0.3"
 wget https://git.io/JZyxV -O - -q | sudo bash -s system $VERSION false
 
 ./stow.sh
+# NOTE: Symlinking the file does not seem to start it after reboot
+# This is likely because the user's filesystem may not be mounted
+# when systemd is initializing services, causing the service definition
+# not to be found.
+sudo cp ./betterlockscreen/betterlockscreen@.service /usr/lib/systemd/system/
 # https://github.com/betterlockscreen/betterlockscreen#systemd-service-lockscreen-after-sleepsuspend
 sudo systemctl enable "betterlockscreen@$USER"
 
