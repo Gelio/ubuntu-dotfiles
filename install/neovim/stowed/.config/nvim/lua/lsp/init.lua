5121d579ffadd4e9829d9cdd2769f35ef3fbf680
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-01-15 10:24:10 +0100

    feat(nvim): use icons for diagnostic signs
    Display icons in the signcolumn for diagnostics instead of displaying
    plain letters.

--- install/neovim/stowed/.config/nvim/lua/lsp/init.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/init.lua
@@ -33,4 +33,15 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagn
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 
+local function use_icons_for_diagnostic_signs()
+	-- https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#change-diagnostic-symbols-in-the-sign-column-gutter
+	local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+	for type, icon in pairs(signs) do
+		local hl = "DiagnosticSign" .. type
+		vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	end
+end
+
+use_icons_for_diagnostic_signs()
+
 require("auto-nvimrc").execute_nvimrcs()
