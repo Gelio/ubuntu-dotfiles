84609819a1e7de15b1e654f373f89f8381eed7b7
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-02-20 21:32:16 +0100

    refactor(nvim): remove `/usr/include` instead of overriding path
    Use the default `path` excluding `/usr/include` instead of overriding
    the `path` option entirely.

--- install/neovim/stowed/.config/nvim/init.lua
+++ install/neovim/stowed/.config/nvim/init.lua
@@ -21,7 +21,7 @@ vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.expandtab = true
 
-vim.o.path = ".,,"
+vim.opt.path:remove("/usr/include")
 
 vim.o.list = true
 -- Source: https://github.com/tjdevries/cyclist.vim
