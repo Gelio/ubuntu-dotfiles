20b5c83608fa45add4c1c5fec9ea76649cdd9ba2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-02-22 08:29:13 +0100

    style(nvim): disable false-positive diagnostic

--- install/neovim/stowed/.config/nvim/lua/plugins.lua
+++ install/neovim/stowed/.config/nvim/lua/plugins.lua
@@ -613,6 +613,8 @@ local function setup_packer(packer_bootstrap)
 				local sources, source_labels = prepare_sources()
 
 				local cmp = require("cmp")
+				-- NOTE: looks like cmp EmmyLua annotations are wrong
+				---@diagnostic disable-next-line:redundant-parameter
 				cmp.setup({
 					snippet = {
 						expand = function(args)
