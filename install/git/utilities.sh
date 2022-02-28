0634207833a1b638e932cc264d90176f17d86ead
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-02-28 13:32:30 +0000

    feat(git): install git-branchless and git-stack utils

--- install/git/utilities.sh
+++ install/git/utilities.sh
@@ -3,3 +3,9 @@ set -euo pipefail
 
 # UI for git
 cargo install gitui
+
+# https://github.com/arxanas/git-branchless
+cargo install --locked git-branchless
+
+# https://github.com/epage/git-stack
+cargo install git-stack
