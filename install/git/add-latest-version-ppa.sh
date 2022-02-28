906d1baafc9eccb82ffdf5f2a4d4b2045d18a495
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-02-28 13:32:44 +0000

    feat(git): add version to switch to latest git ppa
    Allows installing the latest git version using apt.

---
+++ install/git/add-latest-version-ppa.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Source: https://gist.github.com/YuMS/6d7639480b17523f6f01490f285da509
+sudo add-apt-repository -y ppa:git-core/ppa
+sudo apt-get update
+sudo apt-get install git -y
