342ac1f978bfa925dbe6e1cbdcd3a6846ad4de3a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-04-11 09:43:05 +0200

    fix(nvim): disable formatting from the lua language server
    It started conflicting with the formatting from null-ls.

--- install/neovim/stowed/.config/nvim/lua/lsp/lua.lua
+++ install/neovim/stowed/.config/nvim/lua/lsp/lua.lua
@@ -4,7 +4,7 @@ table.insert(runtime_path, "lua/?/init.lua")
 
 local M = {}
 
-M.config = vim.tbl_extend("error", require("lsp.utils").base_config, {
+M.config = vim.tbl_extend("error", require("lsp.utils").base_config_without_formatting, {
 	settings = {
 		Lua = {
 			runtime = {
