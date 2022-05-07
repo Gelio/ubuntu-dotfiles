529314b12c3c96d0dfbde0688868989334d1f297
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-05-07 09:48:07 +0200

    fix(i3): improve forking capabilities when locking the screen
    Fix the suspend command to execute the screen-lock and suspend
    immediately. This is done thanks to adding forking support to the
    lock-screen.sh script. betterlockscreen does not fork by default.

--- install/i3/stowed/.local/bin/lock-screen.sh
+++ install/i3/stowed/.local/bin/lock-screen.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-betterlockscreen -l dim
+fork=0
+if [[ "${1:-}" == "--fork" ]]; then
+  fork=1
+fi
+
+if [[ $fork -eq 1 ]]; then
+  betterlockscreen -l dim &
+  # NOTE: betterlockscreen takes a second or 2 to initialize,
+  # so we want to wait a bit before exiting so lock-screen.sh script
+  # exits when the lock screen is shown.
+  sleep 2
+else
+  betterlockscreen -l dim
+fi
