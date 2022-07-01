b0d57059c169ecd8d4acb05671661763740f9602
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-07-01 20:08:22 +0200

    feat(i3): dynamically generate dpi-specific config files and stow them
    Generate actual config files using gomplate [0]. After the files are
    generated, they are stowed using GNU stow [1].
    
    [0]: https://github.com/hairyhenderson/gomplate
    [1]: https://www.gnu.org/software/stow/

---
+++ install/i3/templated-config/generate-config.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Allows running the script from any directory
+script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+config=${1:-""}
+
+function usage() {
+  echo "Usage: $0 [config]"
+
+  echo "[config] should be a path to a file inside the configs directory"
+}
+
+if [[ -z "$config" ]]; then
+  usage
+  exit 1
+fi
+
+if [[ ! -f "$config" ]]; then
+  echo "$config does not exist"
+  usage
+  exit 2
+fi
+
+templates_dir=./stowed-templates/
+output_dir=stowed-generated
+
+rm "$output_dir" -rf
+gomplate \
+  "--input-dir=${script_dir}/${templates_dir}" \
+  "--output-dir=${script_dir}/${output_dir}/" \
+  --datasource "config=$config"
+
+pushd "$script_dir" >/dev/null
+stow -v --no-folding --restow -t "$HOME" "$output_dir"
