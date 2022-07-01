0fc2d4344b816f13ee82c6fdb6d408fa51603c30
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-07-01 20:12:14 +0200

    fix(nvim): mark parameters as optional
    Use Emmylua annotations to mark some parameters as optional.
    Fixes some of sumneko language server diagnostics.

--- install/neovim/stowed/.config/nvim/lua/plugins.lua
+++ install/neovim/stowed/.config/nvim/lua/plugins.lua
@@ -288,8 +288,8 @@ local function setup_packer(packer_bootstrap)
 				--- Source: https://github.com/nvim-lualine/lualine.nvim/wiki/Component-snippets#truncating-components-in-smaller-window
 				--- @param trunc_width number trunctates component when screen width is less than trunc_width
 				--- @param trunc_len number truncates component to trunc_len number of chars
-				--- @param hide_width number hides component when window width is smaller then hide_width
-				--- @param no_ellipsis boolean whether to disable adding '...' at end after truncation
+				--- @param hide_width? number hides component when window width is smaller then hide_width
+				--- @param no_ellipsis? boolean whether to disable adding '...' at end after truncation
 				--- return function that can format the component accordingly
 				local function trunc(trunc_width, trunc_len, hide_width, no_ellipsis)
 					return function(str)
