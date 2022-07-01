0808169fc378bb6f9f6e40afc8ab55f831dd4906
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-07-01 21:47:37 +0200

    feat(i3): add startx to start scripts for various layouts

--- install/i3/stowed/.screenlayout/home-4k/start.sh
+++ install/i3/stowed/.screenlayout/home-4k/start.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 cd ~/ubuntu-dotfiles/install/i3/templated-config
 ./generate-config.sh configs/hidpi.json
+
+exec startx
--- install/i3/stowed/.screenlayout/regular-dpi/start.sh
+++ install/i3/stowed/.screenlayout/regular-dpi/start.sh
@@ -3,3 +3,5 @@ set -euo pipefail
 
 cd ~/ubuntu-dotfiles/install/i3/templated-config
 ./generate-config.sh configs/regular-dpi.json
+
+exec startx
