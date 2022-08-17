1c75833c0555c6f61a7dc6123cc7775ea537b3d8
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-08-17 09:15:15 +0000

    fix(nvim): linewise comments in TSX
    The `ctx` properties seems to have been renamed for `Comment.nvim`'s
    `prehook`. This caused comments to always be blockwise in TSX files.
    
    I copied the `pre_hook` implementation from
    https://github.com/numToStr/Comment.nvim#-hooks to make the linewise
    comments be linewise comments again.

--- install/neovim/stowed/.config/nvim/lua/plugins.lua
+++ install/neovim/stowed/.config/nvim/lua/plugins.lua
@@ -475,16 +475,18 @@ local function setup_packer(packer_bootstrap)
 			"numToStr/Comment.nvim",
 			config = function()
 				require("Comment").setup({
+					---@param ctx CommentCtx
 					pre_hook = function(ctx)
+						-- Only calculate commentstring for tsx filetypes
 						if vim.bo.filetype == "typescriptreact" then
 							local U = require("Comment.utils")
 
-							-- Detemine whether to use linewise or blockwise commentstring
-							local type = ctx.ctype == U.ctype.line and "__default" or "__multiline"
+							-- Determine whether to use linewise or blockwise commentstring
+							local type = ctx.ctype == U.ctype.linewise and "__default" or "__multiline"
 
 							-- Determine the location where to calculate commentstring from
 							local location = nil
-							if ctx.ctype == U.ctype.block then
+							if ctx.ctype == U.ctype.blockwise then
 								location = require("ts_context_commentstring.utils").get_cursor_location()
 							elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
 								location = require("ts_context_commentstring.utils").get_visual_start_location()
