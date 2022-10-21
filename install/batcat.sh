fb07a67b51f7b31e9a552160a02dafeab5c2b67a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-10-21 10:57:14 +0200

    refactor: install bat from source via cargo
    The version of bat from apt is outdated. Installing from source always
    installs the latest version.
    
    Fixes the problem of using bat for files inside any repository.

--- install/batcat.sh
+++ install/batcat.sh
@@ -1,9 +1,4 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+set -euo pipefail
 
-sudo apt install bat
-
-if [ ! -f /usr/bin/bat ] && [ -f /usr/bin/batcat ]; then
-	sudo ln -s batcat /usr/bin/bat
-	echo "Added a symlink from /usr/bin/bat to batcat"
-fi
+cargo install --locked bat
