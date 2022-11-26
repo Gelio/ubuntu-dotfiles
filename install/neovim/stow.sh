9e8f14029a51cc50f3ff969401d4bbfe748d093c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-11-26 08:43:42 +0100

    refactor(nvim): stow entire Neovim config to ~/.config/nvim
    Symlinking the entire directory helps with automatically including new
    files from ~/.config/nvim into the dotfiles repository. Examples of such
    files are spell files.
    
    After pulling this commit, do
    
    $ rm ~/.config/nvim -r && ./stow.sh && nvim -c PackerCompile

--- install/neovim/stow.sh
+++ install/neovim/stow.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 stow -v --no-folding -R -t "$HOME" stowed
+
+# Stow .config/nvim without folding.
+# This way all new files created by nvim in .config/nvim (like spell files) are
+# added into this repository.
+stow -v -t "$HOME/.config/" config
