6837d908a8f9f6ef1ddc13899a729d02b77a4035
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2022-12-16 18:12:15 +0100

    fix(i3): use xinput prop name instead of ID
    The acceleration speed prop ID changed, probably after upgrading to
    Ubuntu 22.04 LTS. Using the prop name seems like a more stable approach.

--- install/i3/stowed/.screenlayout/home-4k/fix-mouse-pointer-speed.sh
+++ install/i3/stowed/.screenlayout/home-4k/fix-mouse-pointer-speed.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 # Enable pointer acceleration since the default mouse speed is hard to use
-xinput set-prop "pointer:Logitech MX Master 3" 327 0.7
+xinput set-prop "pointer:Logitech MX Master 3" "libinput Accel Speed" 0.7
