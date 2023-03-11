9ac4b7304621e9f40df1757255e0fc27528da003
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-03-11 12:44:06 +0100

    feat(nvim): install lsp_lines.nvim

--- install/neovim/config/nvim/lua/lsp/init.lua
+++ install/neovim/config/nvim/lua/lsp/init.lua
@@ -65,11 +65,6 @@ require("typescript").setup({
 require("lsp.java").setup()
 require("lsp.null-ls").setup()
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-	virtual_text = {
-		source = "always",
-	},
-})
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "single" })
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "single" })
 
