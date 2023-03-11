88eca5ef3f0ac108cd24931ea05d00d1c11c88a9
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-03-11 11:55:54 +0100

    fix(nvim): remove installing jsregexp for LuaSnip
    Running `make install_jsregexp` failed for me (something related to
    luajit). I do not need it and I do not want to spend time fixing it.

--- install/neovim/config/nvim/lua/plugins/editing/completion.lua
+++ install/neovim/config/nvim/lua/plugins/editing/completion.lua
@@ -81,7 +81,6 @@ return {
 			},
 			{
 				"L3MON4D3/LuaSnip",
-				build = "make install_jsregexp",
 				config = function()
 					require("luasnip.loaders.from_vscode").lazy_load()
 					require("luasnip").config.set_config({
