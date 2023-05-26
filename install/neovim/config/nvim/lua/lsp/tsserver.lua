4ccc227d22fab21cf50c76e84f831833c8018550
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-05-26 13:20:01 +0200

    refactor(nvim): disable inlay hints from tsserver
    The code feels cluttered with them.

--- install/neovim/config/nvim/lua/lsp/tsserver.lua
+++ install/neovim/config/nvim/lua/lsp/tsserver.lua
@@ -5,30 +5,8 @@ local utils = require("lsp.utils")
 -- Disable tsserver formatting, use prettierd from null-ls
 M.on_attach = utils.run_all(utils.disable_formatting, utils.on_attach)
 
-local inlay_hints_settings = {
-	includeInlayParameterNameHints = "all",
-	includeInlayParameterNameHintsWhenArgumentMatchesName = false,
-	includeInlayFunctionParameterTypeHints = true,
-	includeInlayVariableTypeHints = true,
-	includeInlayVariableTypeHintsWhenTypeMatchesName = false,
-	includeInlayPropertyDeclarationTypeHints = true,
-	includeInlayFunctionLikeReturnTypeHints = true,
-	includeInlayEnumMemberValueHints = true,
-}
-
 M.config = vim.tbl_extend("force", utils.base_config, {
 	on_attach = M.on_attach,
-
-	-- NOTE: enable inlay hints
-	-- @see https://github.com/lvimuser/lsp-inlayhints.nvim#typescript
-	settings = {
-		typescript = {
-			inlayHints = inlay_hints_settings,
-		},
-		javascript = {
-			inlayHints = inlay_hints_settings,
-		},
-	},
 })
 
 M.setup = function(config)
