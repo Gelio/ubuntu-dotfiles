74ce2112f7fbefdc2f20aeac3f2dc2cb1338b349
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-10-09 13:04:03 +0200

    fix(nvim): Lua workspace configuration
    Problem: Ever since I started using lazy.nvim, the Lua language server workspace
    (used to load definitions from) only loaded the files from plugins that
    were loaded at the time of Neovim initialization. Most plugins are lazy
    loaded. They are not loaded at the time of Neovim initialization. Lua-LS
    does not load their types and does not index them.
    
    Solution: Find all plugin directories and feed those manually as the
    workspace for Lua-LS to load. Use `runtime.pathStrict` so Lua-LS only
    indexes files the same way Neovim would (only the first level of
    plugins).
    
    This way `require("cmp")` resolves to the `nvim-cmp/lua/cmp/init.lua`
    file.

--- install/neovim/config/nvim/lua/lsp/lua.lua
+++ install/neovim/config/nvim/lua/lsp/lua.lua
@@ -1,28 +1,49 @@
-local runtime_path = vim.split(package.path, ";")
-table.insert(runtime_path, "lua/?.lua")
-table.insert(runtime_path, "lua/?/init.lua")
-
 local M = {}
 
+local function get_all_lazy_plugin_lua_dirs()
+	local plugins = {}
+	local lazy_plugins_dir_path = vim.env.HOME .. "/.local/share/nvim/lazy"
+
+	for name, type in vim.fs.dir(lazy_plugins_dir_path) do
+		if type == "directory" then
+			table.insert(plugins, vim.fs.joinpath(lazy_plugins_dir_path, name, "lua"))
+		end
+	end
+
+	return plugins
+end
+
 M.config = vim.tbl_extend("error", require("lsp.utils").base_config_without_formatting, {
-	settings = {
-		Lua = {
-			runtime = {
-				version = "LuaJIT",
-				path = runtime_path,
+	on_init = function(client)
+		local plugins = get_all_lazy_plugin_lua_dirs()
+
+		client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+			Lua = {
+				runtime = {
+					version = "LuaJIT",
+					path = {
+						"?.lua",
+						"?/init.lua",
+						"lua/?.lua",
+						"lua/?/init.lua",
+					},
+					pathStrict = true,
+				},
+				workspace = {
+					library = vim.list_extend(plugins, {
+						vim.env.VIMRUNTIME,
+						vim.opt.runtimepath:get()[1],
+					}),
+					checkThirdParty = false,
+				},
+				telemetry = {
+					enable = false,
+				},
 			},
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				library = vim.api.nvim_get_runtime_file("", true),
-				checkThirdParty = false,
-			},
-			telemetry = {
-				enable = false,
-			},
-		},
-	},
+		})
+
+		client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+	end,
 })
 
 return M
