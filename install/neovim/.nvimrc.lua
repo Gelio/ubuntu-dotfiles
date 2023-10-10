5df8e1005f38438f074fd2b0a5d6aa4239df15c0
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-10-10 14:49:47 +0200

    chore(nvim): set Lua-LS cwd to config/nvim directory
    Helps with Lua-LS indexing the current runtime files.

--- install/neovim/.nvimrc.lua
+++ install/neovim/.nvimrc.lua
@@ -8,3 +8,10 @@ null_ls.register(null_ls.builtins.diagnostics.selene.with({
 		return nvimrc_dir
 	end,
 }))
+
+local lua_ls_config = require("lsp.lua").config
+require("lspconfig").lua_ls.setup(vim.tbl_extend("error", lua_ls_config, {
+	root_dir = function()
+		return vim.fs.joinpath(nvimrc_dir, "config", "nvim")
+	end,
+}))
