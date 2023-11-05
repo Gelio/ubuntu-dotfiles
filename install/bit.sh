4768da7457d7f633d9fe2f432d3f0005a28efcbf
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-11-05 13:36:52 +0100

    chore: remove bit and navi install files
    I am not using these tools.

--- install/bit.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-# https://github.com/chriswalz/bit
-# An alternative client for git
-curl -sf https://gobinaries.com/chriswalz/bit | sh
-bit complete
-echo "Type bit then press <ENTER> to show interactive prompt"
