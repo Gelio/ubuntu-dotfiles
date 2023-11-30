00d53dfa26edd5426f8047064206bc87bc054c5d
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2023-11-30 13:25:25 +0100

    refactor(nvim): disable vim-markdown indent
    I do not like its behavior. I prefer the default behavior.

--- universal/neovim/config/nvim/lua/plugins/lang/markdown.lua
+++ universal/neovim/config/nvim/lua/plugins/lang/markdown.lua
@@ -41,6 +41,18 @@ return {
 		"plasticboy/vim-markdown",
 		config = function()
 			vim.g.vim_markdown_no_extensions_in_markdown = true
+			vim.g.vim_markdown_auto_insert_bullets = 0
+			vim.g.vim_markdown_new_list_item_indent = 0
+
+			local augroup = vim.api.nvim_create_augroup("VimMarkdownOverrides", {})
+			vim.api.nvim_create_autocmd("FileType", {
+				pattern = "markdown",
+				group = augroup,
+				callback = function()
+					-- NOTE: vim-markdown's indentexpr is annoying to work with
+					vim.bo.indentexpr = ""
+				end,
+			})
 		end,
 		ft = "markdown",
 		dependencies = {
