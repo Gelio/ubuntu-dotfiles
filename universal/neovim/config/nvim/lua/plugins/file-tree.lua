eee2e9914df726d63870cf1174b80f08f938abc4
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-03-12 12:19:47 +0100

    feat(nvim): show directory path in oil.nvim winbar

--- universal/neovim/config/nvim/lua/plugins/file-tree.lua
+++ universal/neovim/config/nvim/lua/plugins/file-tree.lua
@@ -9,6 +9,9 @@ return {
 				["<C-v>"] = "actions.select_vsplit",
 				["<C-x>"] = "actions.select_split",
 			},
+			win_options = {
+				winbar = "%{v:lua.require('oil').get_current_dir()}",
+			},
 		},
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
