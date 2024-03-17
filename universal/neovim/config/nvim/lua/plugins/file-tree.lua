49da282ac5157f36643b7cbf646fe850e38f32b2
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-03-17 13:15:20 +0100

    refactor(nvim): improve oil.nvim winbar appearance
    Use StatusLine highlight group for the final directory so it stands out
    more.
    
    Shorten the directory path to be relative to the home directory (if
    possible).

--- universal/neovim/config/nvim/lua/plugins/file-tree.lua
+++ universal/neovim/config/nvim/lua/plugins/file-tree.lua
@@ -1,18 +1,28 @@
 return {
 	{
 		"stevearc/oil.nvim",
-		opts = {
-			columns = {
-				"icon",
-			},
-			keymaps = {
-				["<C-v>"] = "actions.select_vsplit",
-				["<C-x>"] = "actions.select_split",
-			},
-			win_options = {
-				winbar = "%{v:lua.require('oil').get_current_dir()}",
-			},
-		},
+		opts = function()
+			-- selene: allow(global_usage)
+			function _G.oil_winbar()
+				local current_dir = require("oil").get_current_dir()
+				local head = vim.fn.fnamemodify(current_dir, ":~:h:h")
+				local tail = vim.fn.fnamemodify(current_dir, ":h:t")
+				return head .. "/%#StatusLine#" .. tail .. "/"
+			end
+
+			return {
+				columns = {
+					"icon",
+				},
+				keymaps = {
+					["<C-v>"] = "actions.select_vsplit",
+					["<C-x>"] = "actions.select_split",
+				},
+				win_options = {
+					winbar = "%{%v:lua.oil_winbar()%}",
+				},
+			}
+		end,
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		keys = {
 			{ "-", "<cmd>Oil<CR>", desc = "Open parent directory" },
