4bde4cdd5275ed30e5a500fa304b1b535df3b41c
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2024-06-01 17:11:24 +0200

    refactor(nvim): remove custom rainbow-delimiters highlights
    Use the highlight groups set by gruvbox-material.nvim.

--- universal/neovim/config/nvim/lua/plugins/treesitter.lua
+++ universal/neovim/config/nvim/lua/plugins/treesitter.lua
@@ -140,16 +140,6 @@ return {
 					[""] = "rainbow-delimiters",
 					lua = "rainbow-blocks",
 				},
-				highlight = {
-					"RainbowLevel0",
-					"RainbowLevel1",
-					"RainbowLevel2",
-					"RainbowLevel3",
-					"RainbowLevel4",
-					"RainbowLevel5",
-					"RainbowLevel6",
-					"RainbowLevel7",
-				},
 			}
 		end,
 	},
