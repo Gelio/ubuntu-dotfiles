4dbf9c091468da1c24adcda3a3e8741558443c13
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-03-19 09:27:49 +0100

    feat(nvim): add CopilotChat.nvim plugin
    Use CopilotChat.nvim for GitHub Copilot chat mode.

--- universal/neovim/config/nvim/lua/plugins/editing/copilot.lua
+++ universal/neovim/config/nvim/lua/plugins/editing/copilot.lua
@@ -1,3 +1,19 @@
 return {
-	{ "github/copilot.vim" },
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		dependencies = {
+			{ "github/copilot.vim" },
+			{ "nvim-lua/plenary.nvim", branch = "master" },
+		},
+		build = "make tiktoken",
+		opts = {},
+		cmd = {
+			"CopilotChat",
+			"CopilotChatOpen",
+			"CopilotChatToggle",
+			"CopilotChatPrompts",
+			"CopilotChatAgents",
+			"CopilotChatModels",
+		},
+	},
 }
