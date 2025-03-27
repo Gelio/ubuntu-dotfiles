bd49acac7442def982a17750db614e5044664b40
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-03-27 10:08:42 +0100

    refactor(macos): apply all yabai rules on restart
    Yabai rules do not apply automatically after `yabai --restart-service`.
    Let's change the setup script to automatically apply them.

--- macos/core/setup-yabai.sh
+++ macos/core/setup-yabai.sh
@@ -5,6 +5,9 @@ set -euo pipefail
 echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 "$(which yabai)" | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
 
 echo "You will get a prompt to enable Yabai in macOS accessibility settings."
-echo "If yabai does not restart, you need to run 'yabai --restart-service' again after changing those accessibility settings."
+echo "If yabai does not restart, you need to run this script again after changing those accessibility settings."
 set -x
 yabai --restart-service
+
+# Re-apply all rules after restart
+yabai -m rule --apply
