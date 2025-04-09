e2005c3e17b2c529515d676a53eed1b5d13eaacd
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-04-09 11:07:30 +0200

    fix(macos): add sleep to setup yabai to account for yabai restarting

--- macos/core/setup-yabai.sh
+++ macos/core/setup-yabai.sh
@@ -9,5 +9,6 @@ echo "If yabai does not restart, you need to run this script again after changin
 set -x
 yabai --restart-service
 
-# Re-apply all rules after restart
+# Re-apply all rules after restart, but give it a few seconds to settle
+sleep 4
 yabai -m rule --apply
