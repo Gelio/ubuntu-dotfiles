6065dd8ddf621aa3848dc8519744c5a4e3256d7e
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-05-22 11:32:49 +0200

    refactor(nvim): rename volar to vue_ls
    The name was changed in nvim-lspconfig.

--- universal/neovim/config/nvim/lua/lsp/init.lua
+++ universal/neovim/config/nvim/lua/lsp/init.lua
@@ -30,7 +30,7 @@ local server_configs = {
 	css_variables = default_server_config,
 	-- https://github.com/coder3101/protols
 	protols = default_server_config,
-	volar = default_server_config,
+	vue_ls = default_server_config,
 }
 
 require("mason-lspconfig").setup({
