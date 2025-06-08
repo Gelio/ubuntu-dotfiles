a6bf9958c97f8892ed5e731977d728ce09579332
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-06-08 17:24:35 +0200

    refactor(nvim): use before_init to lazy-load lua_ls config
    Use `before_init`, which is executed only once, and is simpler to use to
    change the `settings` object of the LSP configuration. It does not
    require sending a `workspace/didChangeConfiguration` notification,
    because that happens automatically by Neovim later.

--- universal/neovim/config/nvim/lsp/lua_ls.lua
+++ universal/neovim/config/nvim/lsp/lua_ls.lua
@@ -11,42 +11,45 @@ local function get_all_lazy_plugin_lua_dirs()
 	return plugins
 end
 
-return vim.tbl_extend("error", require("lsp.utils").base_config_without_formatting, {
-	settings = {
-		Lua = {
-			diagnostics = {
-				globals = { "vim" },
+local settings = {
+	Lua = {
+		diagnostics = {
+			globals = { "vim" },
+		},
+		runtime = {
+			version = "LuaJIT",
+			path = {
+				"?.lua",
+				"?/init.lua",
+				"lua/?.lua",
+				"lua/?/init.lua",
 			},
-			runtime = {
-				version = "LuaJIT",
-				path = {
-					"?.lua",
-					"?/init.lua",
-					"lua/?.lua",
-					"lua/?/init.lua",
-				},
-				pathStrict = true,
-			},
-			telemetry = {
-				enable = false,
-			},
+			pathStrict = true,
+		},
+		telemetry = {
+			enable = false,
 		},
 	},
-	on_init = function(client)
-		-- NOTE: read the list of plugins lazily, so we don't incur this cost on each startup
-		local plugins = get_all_lazy_plugin_lua_dirs()
+}
 
-		client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-			Lua = {
-				workspace = {
-					library = vim.list_extend(plugins, {
-						vim.env.VIMRUNTIME,
-					}),
-					checkThirdParty = false,
-				},
-			},
-		})
+local function set_plugin_dirs_in_settings()
+	local plugins = get_all_lazy_plugin_lua_dirs()
 
-		client:notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-	end,
-})
+	settings.Lua.workspace = vim.tbl_deep_extend("force", settings.Lua.workspace or {}, {
+		library = vim.list_extend(plugins, {
+			vim.env.VIMRUNTIME,
+		}),
+		checkThirdParty = false,
+	})
+end
+
+return vim.tbl_extend(
+	"error",
+	require("lsp.utils").base_config_without_formatting,
+	{
+		settings = settings,
+		-- NOTE: read the list of plugins lazily,
+		-- so we don't incur this cost on each nvim startup
+		before_init = set_plugin_dirs_in_settings,
+	}
+)
