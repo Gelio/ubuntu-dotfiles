08127c03d6db52049da09d53e585dff8f8537c39
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-06-08 17:26:04 +0200

    refactor(nvim): simplify config for jsonls LSP client
    Do not extend the special config that disables formatting. conform.nvim
    knows to use prettier for json files so jsonls formatting is never used
    anyway. I haven't noticed any conflicts.

--- universal/neovim/config/nvim/lsp/jsonls.lua
+++ universal/neovim/config/nvim/lsp/jsonls.lua
@@ -1,12 +1,7 @@
-return vim.tbl_extend(
-	"error",
-	-- Conflicts with prettier formatting in JSON files.
-	require("lsp.utils").base_config_without_formatting,
-	{
-		settings = {
-			json = {
-				schemas = require("schemastore").json.schemas(),
-			},
+return {
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
 		},
-	}
-)
+	},
+}
