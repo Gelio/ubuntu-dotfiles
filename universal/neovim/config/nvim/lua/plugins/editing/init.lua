c96b42826da82189bbf1a1fc630c419af5a72dab
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-06-09 11:52:19 +0200

    perf(nvim): lazy-load Comment.nvim

--- universal/neovim/config/nvim/lua/plugins/editing/init.lua
+++ universal/neovim/config/nvim/lua/plugins/editing/init.lua
@@ -88,6 +88,7 @@ return {
 	},
 	{
 		"numToStr/Comment.nvim",
+		keys = { "gc", "gb" },
 		opts = function()
 			return {
 				pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
