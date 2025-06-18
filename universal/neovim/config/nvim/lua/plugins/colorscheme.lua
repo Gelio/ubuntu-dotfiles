67b2fec326be1fa467e6c83e5b57908b66fd9fcf
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-06-18 11:22:21 +0200

    feat(nvim): use brighter color for line numbers

--- universal/neovim/config/nvim/lua/plugins/colorscheme.lua
+++ universal/neovim/config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,13 @@ return {
 	{
 		"rebelot/kanagawa.nvim",
 		config = function()
+			require("kanagawa").setup({
+				overrides = function(colors)
+					return {
+						LineNr = { fg = colors.theme.ui.fg_dim },
+					}
+				end,
+			})
 			vim.cmd.colorscheme("kanagawa")
 		end,
 	},
