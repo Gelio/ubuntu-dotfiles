ce0e29a54f9c4c65507becf6faa3d10be2fd1f2a
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-07-02 11:43:00 +0200

    feat(nvim): add [c and ]c mapping to oil buffers
    Jump between git-modified entries.

--- universal/neovim/config/nvim/lua/plugins/file-tree.lua
+++ universal/neovim/config/nvim/lua/plugins/file-tree.lua
@@ -10,6 +10,19 @@ return {
 				return head .. "/%#StatusLine#" .. tail .. "/"
 			end
 
+			-- NOTE: must be the same as https://github.com/refractalize/oil-git-status.nvim/blob/4b5cf53842c17a09420919e655a6a559da3112d7/lua/oil-git-status.lua#L2C1-L2C66
+			local namespace = vim.api.nvim_create_namespace("oil-git-status")
+
+			---@param hl_group string
+			local function is_unmodified_hl_group(hl_group)
+				return hl_group == "OilGitStatusIndexUnmodified" or hl_group == "OilGitStatusWorkingTreeUnmodified"
+			end
+
+			---@param extmark vim.api.keyset.get_extmark_item
+			local function jump_to_extmark_line(extmark)
+				vim.cmd("normal! " .. extmark[2] + 1 .. "G")
+			end
+
 			return {
 				columns = {
 					"icon",
@@ -19,6 +32,61 @@ return {
 					["<C-v>"] = "actions.select_vsplit",
 					["<C-x>"] = "actions.select_split",
 					["<C-s>"] = "actions.select_split",
+					["[c"] = {
+						callback = function()
+							local current_line_number = vim.fn.line(".") - 1
+							if current_line_number == 0 then
+								return
+							end
+
+							local extmarks = vim.api.nvim_buf_get_extmarks(
+								0,
+								namespace,
+								0,
+								{ current_line_number - 1, -1 },
+								{ details = true }
+							)
+							local first_modified_extmark = vim.iter(extmarks):rev():find(
+								---@param extmark vim.api.keyset.get_extmark_item
+								function(extmark)
+									return not is_unmodified_hl_group(extmark[4].sign_hl_group)
+								end
+							)
+
+							if first_modified_extmark ~= nil then
+								jump_to_extmark_line(first_modified_extmark)
+							end
+						end,
+						desc = "Previous changed entry",
+					},
+					["]c"] = {
+						callback = function()
+							local current_line_number = vim.fn.line(".") - 1
+							local last_line_number = vim.fn.line("$") - 1
+							if current_line_number == last_line_number then
+								return
+							end
+
+							local extmarks = vim.api.nvim_buf_get_extmarks(
+								0,
+								namespace,
+								{ current_line_number + 1, 0 },
+								-1,
+								{ details = true }
+							)
+							local first_modified_extmark = vim.iter(extmarks):find(
+								---@param extmark vim.api.keyset.get_extmark_item
+								function(extmark)
+									return not is_unmodified_hl_group(extmark[4].sign_hl_group)
+								end
+							)
+
+							if first_modified_extmark ~= nil then
+								jump_to_extmark_line(first_modified_extmark)
+							end
+						end,
+						desc = "Next changed entry",
+					},
 				},
 				win_options = {
 					winbar = "%{%v:lua.oil_winbar()%}",
