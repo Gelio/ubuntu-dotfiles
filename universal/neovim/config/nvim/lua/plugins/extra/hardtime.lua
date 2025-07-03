ff4b4217c14dd953c75b9042a56bc8371b3bdb82
Grzegorz Rozdzialik <voreny.gelio@gmail.com>
2025-07-03 12:53:30 +0200

    refactor(nvim): enable mouse in hardtime.nvim

--- universal/neovim/config/nvim/lua/plugins/extra/hardtime.lua
+++ universal/neovim/config/nvim/lua/plugins/extra/hardtime.lua
@@ -2,6 +2,10 @@ return {
 	{
 		"m4xshen/hardtime.nvim",
 		dependencies = { "MunifTanjim/nui.nvim" },
-		opts = {},
+		opts = {
+			-- NOTE: I am often using the mouse during code review and when screen sharing.
+			-- I don't find it useful to disable the mouse.
+			disable_mouse = false,
+		},
 	},
 }
